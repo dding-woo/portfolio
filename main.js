43b3b2fa82b6a81e23e846f56ccb43b0a7c19fea
Daon98 <70466220+Daon98@users.noreply.github.com>
2021-06-18 21:23:40 +0900

    first commit

--- main.js
+++ main.js
@@ -1,127 +1,141 @@
 'use strict';
 
-const navar = document.querySelector('#navbar');
+const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
-document.addEventListener('scroll',() =>{
-    console.log(window.scrollY);    
-    console.log(`navbar:${navbarHeight}`);
-    
-    if (window.scrollY>navbarHeight){
+document.addEventListener('scroll', () => {
+
+    if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar--dark');
     }
-    else{
+    else {
         navbar.classList.remove('navbar--dark');
     }
 });
 
+
+
+
 //handle scrolling when tapping on the navbar menu
-const navbarMenu= document.querySelector('.navbar__menu');
+const navbarMenu = document.querySelector('.navbar__menu');
 const navbarMenItem = document.querySelectorAll('.navbar__menu__item');
 
-navbarMenu.addEventListener('click',(event)=>{
+
+navbarMenu.addEventListener('click', (event) => {
     const target = event.target;
-    const link = target.dataset.link;   
-   
-    
-    if(link==null){
+    const link = target.dataset.link;
+
+
+    if (link == null) {
         return;
     }
-  
-    scrollIntoView(link);
-}); 
 
+    scrollIntoView(link);
+});
+
+
+// navbar-menu toggle 
+const toggleBtn = document.querySelector('.navbar__toggle-btn');
+toggleBtn.addEventListener('click', () => {
+
+    navbarMenu.classList.toggle('navbar__menu-toggle');
+
+
+});
 
 //handle scrolling  when contact me button 
 
 const homecontacButton = document.querySelector('.home__contact');
-homecontacButton.addEventListener('click',()=>{
-    
-    
+homecontacButton.addEventListener('click', () => {
+
+
     scrollIntoView('#contact');
 
-}); 
+});
 
 //home-opacity when scrolling 
-   
+
 const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll',()=>{  
-    home.style.opacity= 1- window.scrollY/homeHeight;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
-document.addEventListener('scroll',()=>{
-    homecontacButton.style.opacity= 1- window.scrollY/homeHeight;
-   });
+document.addEventListener('scroll', () => {
+    homecontacButton.style.opacity = 1 - window.scrollY / homeHeight;
+});
 //contactme btn 
-homecontacButton.addEventListener('mouseenter',()=>{
-    homecontacButton.style.opacity=1;
+homecontacButton.addEventListener('mouseenter', () => {
+    homecontacButton.style.opacity = 1;
 });
-homecontacButton.addEventListener('mouseleave',()=>{
-    homecontacButton.style.opacity= 1- window.scrollY/homeHeight;
+homecontacButton.addEventListener('mouseleave', () => {
+    homecontacButton.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // arrow up Button when click 
 const arrowUpbtn = document.querySelector('.arrowup__btn');
 
-document.addEventListener('scroll',()=>{
+document.addEventListener('scroll', () => {
 
-       if( window.scrollY>homeHeight){
-        arrowUpbtn.style.opacity=1;
-        
-       }else{
-        arrowUpbtn.style.opacity=0;
-       }
+    if (window.scrollY > homeHeight) {
+        arrowUpbtn.style.opacity = 1;
+
+    } else {
+        arrowUpbtn.style.opacity = 0;
+    }
 });
-arrowUpbtn.addEventListener('click',()=>{
-    
+arrowUpbtn.addEventListener('click', () => {
+
     scrollIntoView('#home');
 });
 
-//categories btn
+//skill value graph
+const skillValue = document.querySelectorAll(".skill .skill__bar .skill__value")
 
-const workBtnContaniner = document.querySelector('.work__categories');
-const projectContaniner = document.querySelector('.work__projects'); 
-const projects = document.querySelectorAll('.project');
-
-workBtnContaniner.addEventListener('click',(event)=>{
-    const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
-   
-    if(filter==null){
-        return;
-    }
-  //Remove  selection from the previous item and select the new one.
-    const active = document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
-    const target = event.target.nodeName ==='BUTTON' ?   event.target : event.target.parentNode;
-    target.classList.add('selected');
-
-    projectContaniner.classList.add('ani--out');
-    setTimeout(()=>{
-        projects.forEach((project)=>{
-            console.log(project.dataset.type);  
-    
-         if(filter==='*' || filter===project.dataset.type) {
-            project.classList.remove('invisible');   //All이거나 데이터 타입과 필터가 일치하는 것은 보여줘야하니까 안보여지는 클래스는 제거하고
-         }else{
-            project.classList.add('invisible');// 데이터 타입과 필터가 일치 하지 않는 것들은 안보여야 하니까 얀보이게 하는 클래스를 추가.
-         } 
+document.addEventListener("scroll", () => {
+    if (window.scrollY > 1490) {
+        skillValue.forEach((graph) => {
+            graph.classList.add("on");
         });
-    
-        projectContaniner.classList.remove('ani--out');
+    }
+});
+// slider portpolio img
 
-    },300);
-  
+const slider = document.querySelector(".project__slider");
+const slide = document.querySelectorAll(".project");
+let currentIndex = 0;
+const sliderCount = slide.length;
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+prevBtn.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        moveslide(currentIndex - 1);
+    } else {
+        moveslide(currentIndex + 2);
+
+    }
+});
+nextBtn.addEventListener("click", () => {
+    if (currentIndex < sliderCount - 1) {
+        moveslide(currentIndex + 1)
+    } else {
+        moveslide(0);
+    }
 });
 
+function moveslide(num) {
+    slider.style.left = -num * 1230 + "px";
+    currentIndex = num;
+
+}
 
 
 
 
 //scrollIntoView fucntion
 
-function scrollIntoView(selector){
-    const scrollTo=document.querySelector(selector);
-   scrollTo.scrollIntoView({behavior: "smooth"});
-   }
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: "smooth" });
+}
 
- 
